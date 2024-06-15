bd1873c012d6508eefd53cba16dc99d0b9a7da2d
Chester J. Ayala <chesterayala.ca@gmail.com>
2024-06-15 09:51:22 +0800

    Add ObjectID hashing

--- AlgoliaCrawler/Model/PageIndex.cs
+++ AlgoliaCrawler/Model/PageIndex.cs
@@ -1,8 +1,27 @@
-﻿namespace AlgoliaCrawler.Models
+﻿using Murmur;
+using System.Text;
+
+namespace AlgoliaCrawler.Models
 {
     public sealed class PageIndex
     {
+        private string _objectID;
+
         public string Title { get; set; }
         public string Url { get; set; }
+        public string ObjectID
+        {
+            get
+            {
+                if (_objectID == null)
+                {
+                    var hasher = MurmurHash.Create32();
+                    var hashBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(Url));
+                    var hash = BitConverter.ToUInt32(hashBytes, 0);
+                    _objectID = hash.ToString("X");
+                }
+                return _objectID;
+            }
+        }
     }
 }
