14e5ed5f12cabcb1d4e1fde79af8ec7a3efcb043
Chester J. Ayala <chesterayala.ca@gmail.com>
2024-06-15 10:52:27 +0800

    Handle HTML doc type for Sitemap XML parsing

--- AlgoliaCrawler/HttpClientExtensions.cs
+++ AlgoliaCrawler/HttpClientExtensions.cs
@@ -13,7 +13,13 @@ namespace AlgoliaCrawler
                 if (response.StatusCode == HttpStatusCode.NotFound)
                     return null;
 
+                var contentType = response.Content.Headers.ContentType.MediaType;
+
+                if (contentType.Contains("html"))
+                    return null;
+
                 response.EnsureSuccessStatusCode();
+
                 return await response.Content.ReadAsStringAsync();
             }
             catch (Exception)
